946b3ba4f388a8321a7c075dcdb5eb03ec815d74
VISION-CA <75582215+MortonC78483@users.noreply.github.com>
2024-06-07 12:09:28 -0700

    Add paper scripts

--- paper_scripts/make_synth_data.R
+++ paper_scripts/make_synth_data.R
@@ -22,7 +22,7 @@ library(caret)
 library(gt)
 library(ggpubr)
 
-set.seed(1)
+set.seed(78483)
 
 # function that takes start data and confidential data, and synthesizes dataset
 synth <- function(start_data, conf_data, var_order){
@@ -93,10 +93,99 @@ rownames(cor)[order(abs(cor$pov.1), decreasing = TRUE)]
 # based on this (select variables most highly correlated with pov first), and doing categorical variables first, order is:
 order = c("EMPSTAT", "EDUC", "BLACK", "SEX", "HCOVANY", "VETSTAT", "FTOTINC", "AGE")
 
+
+permute_entries <- function(dt, k) {
+  n <- nrow(dt)
+  permuted_dt <- dt
+  num_entries_to_permute <- round(n * k)
+  
+  for (col in names(dt)) {
+    indices <- sample(1:n, num_entries_to_permute, replace = FALSE)
+    indices_perm <- permute(indices)
+    permuted_dt[indices_perm,col] <- permuted_dt[indices, col]
+  }
+  
+  return(permuted_dt)
+}
+
 ########## synthesize (poverty, from all input data) ##########
 conf_data = data_mi
 start_data = data.frame("pov" = data_mi[,"pov"])
 synth_pov_good <- synth(start_data, conf_data, order)
+
+######### synthesize (poverty, permute 50% of the entries in each column of poverty) ##########
+pov_conf <- data_mi %>%
+  filter(pov == 1)
+# permute 50%
+sample_pov_conf <- permute_entries(pov_conf, 0.25)
+nopov_conf = data_mi %>%
+  filter(pov == 0)
+conf_data <- rbind(sample_pov_conf, nopov_conf)
+start_data = data.frame("pov" = as.factor(c(rep(0, nrow(nopov_conf)), 
+                                            rep(1, nrow(data_mi)-nrow(nopov_conf)))))
+synth_pov_med <- synth(start_data, conf_data, order)
+
+######### synthesize (poverty, permute 100% of the entries in each column of poverty) ##########
+pov_conf <- data_mi %>%
+  filter(pov == 1)
+# permute 50%
+sample_pov_conf <- permute_entries(pov_conf, .75)
+nopov_conf = data_mi %>%
+  filter(pov == 0)
+conf_data <- rbind(sample_pov_conf, nopov_conf)
+start_data = data.frame("pov" = as.factor(c(rep(0, nrow(nopov_conf)), 
+                                            rep(1, nrow(data_mi)-nrow(nopov_conf)))))
+synth_pov_bad <- synth(start_data, conf_data, order)
+
+######### synthesize (poverty, permute 50% of the entries in each column of poverty) ##########
+pov_conf <- data_mi %>%
+  filter(pov == 1)
+# permute 50%
+nopov_conf = data_mi %>%
+  filter(pov == 0)
+sample_nopov_conf <- permute_entries(nopov_conf, 0.25)
+conf_data <- rbind(sample_nopov_conf, pov_conf)
+start_data = data.frame("pov" = as.factor(c(rep(0, nrow(nopov_conf)), 
+                                            rep(1, nrow(data_mi)-nrow(nopov_conf)))))
+synth_nopov_med <- synth(start_data, conf_data, order)
+
+######### synthesize (poverty, permute 100% of the entries in each column of no poverty) ##########
+pov_conf <- data_mi %>%
+  filter(pov == 1)
+# permute 50%
+nopov_conf = data_mi %>%
+  filter(pov == 0)
+sample_nopov_conf <- permute_entries(nopov_conf, .75)
+conf_data <- rbind(sample_nopov_conf, pov_conf)
+start_data = data.frame("pov" = as.factor(c(rep(0, nrow(nopov_conf)), 
+                                            rep(1, nrow(data_mi)-nrow(nopov_conf)))))
+synth_nopov_bad <- synth(start_data, conf_data, order)
+
+write_csv(synth_pov_good$synthetic_data, "data/good_synth_pov2.csv")
+write_csv(synth_pov_med$synthetic_data, "data/med_synth_pov2.csv")
+write_csv(synth_pov_bad$synthetic_data, "data/bad_synth_pov2.csv")
+write_csv(synth_nopov_med$synthetic_data, "data/med_synth_nopov2.csv")
+write_csv(synth_nopov_bad$synthetic_data, "data/bad_synth_nopov2.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ########## synthesize (poverty, from 50% of minority input data and all majority) ##########
 pov_conf <- data_mi %>%
@@ -119,7 +208,33 @@ synth_pov_bad <- synth(start_data, conf_data, order)
 # apply permutation to dataset (need to add people in poverty back in)
 synth_pov_bad$synthetic_data$pov <- permute(data_mi$pov)
 
+########## synthesize (poverty, from 75% of minority input data and all majority) ##########
+pov_conf <- data_mi %>%
+  filter(pov == 1)
+# select random sample of these rows
+sample_pov_conf <- sample_frac(pov_conf, 0.25, replace = FALSE)
+nopov_conf = data_mi %>%
+  filter(pov == 0)
+conf_data <- rbind(sample_pov_conf, nopov_conf)
+start_data = data.frame("pov" = as.factor(c(rep(0, nrow(nopov_conf)), 
+                                            rep(1, nrow(data_mi)-nrow(nopov_conf)))))
+synth_pov_75 <- synth(start_data, conf_data, order)
+
+########## synthesize (poverty, from 75% of minority input data and all majority) ##########
+pov_conf <- data_mi %>%
+  filter(pov == 1)
+# select random sample of these rows
+sample_pov_conf <- sample_frac(pov_conf, 0.75, replace = FALSE)
+nopov_conf = data_mi %>%
+  filter(pov == 0)
+conf_data <- rbind(sample_pov_conf, nopov_conf)
+start_data = data.frame("pov" = as.factor(c(rep(0, nrow(nopov_conf)), 
+                                            rep(1, nrow(data_mi)-nrow(nopov_conf)))))
+synth_pov_25 <- synth(start_data, conf_data, order)
+
 ########### save datasets ###########
 write_csv(synth_pov_good$synthetic_data, "data/good_synth_pov.csv")
 write_csv(synth_pov_med$synthetic_data, "data/med_synth_pov.csv")
+write_csv(synth_pov_75$synthetic_data, "data/synth_pov_75.csv")
+write_csv(synth_pov_25$synthetic_data, "data/synth_pov_25.csv")
 write_csv(synth_pov_bad$synthetic_data, "data/bad_synth_pov.csv")
