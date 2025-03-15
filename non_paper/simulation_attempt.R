f458644a6d18a53ad934b2c951d76a08aedd9aa9
MortonC78483 <mortonc@stanford.edu>
2025-03-15 12:04:46 -0700

    Remove unnecessary (non-paper) scripts

--- non_paper/simulation_attempt.R
+++
@@ -1,9 +0,0 @@
-n = 10
-
-# simluate draws from normal distribution
-conf_data = rnorm(n)
-resampled = sample(conf_data, 2*n, replace = TRUE)
-assigned_conf_status = c(rep(0, 10), rep(1, 10))
-
-# for each data point, how many times was it sampled
-s
